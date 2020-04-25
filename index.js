49d362edb6d1f9acd9c75aa4d91dbeabbfb4b752
Alexander Thiel <me@alexthiel.de>
2020-04-25 12:00:54 +0200

    Increases interval to once a day

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let server;
 
 app.use(createMiddleware({
     url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQGa4pEgEAIHiM3OHYHHhhu_M90t3PzU2S6rQVT-DVxnr9w0-srVOCBn4asEKaKmZCsfKLx-VF4uYL7/pub?gid=0&single=true&output=tsv",
-    interval: 60000,
+    interval: 1000 * 3600 * 24, //Once a day (Updates are typically triggered manually)
     onError: error => {
         console.error(error);
         server.close();
